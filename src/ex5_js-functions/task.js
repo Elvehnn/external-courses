5b4205e6ac51d4d664028e9bbbb7a5c2dde2a5a1
Elena Shashina <elvehnn@yandex.ru>
2021-04-09 20:00:49 +0300

    fixed ex.5

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,92 +1,38 @@
 const calculator = Calculator();
 
-const Calculator = {
-  result: 0,
-
-  getResult: function() {
-    return calculator.result;
-  },
-
-  reset: function() {
-    calculator.result = 0;
-    return calculator.result;
-  },
-    
-  add: function(a) {
-    var acc = calculator.result;
-    if (a !== undefined) {
-      acc += a;
-      calculator.result = acc;
-    }
-    let n = (b) => {
-      if (b !== undefined) {
-        acc += b;
-        calculator.result = acc;
-      } 
-    return n;
-    }
-    return n;
-  },
-
-  subtract: function(a) {
-    var acc;
-    if (a === undefined) {
-      acc = 0;
-    } else {
-      acc = calculator.result - a;
-      calculator.result = acc;
-    }
-    let n = (b) => {
-      if (b !== undefined) {
-        acc -= b;
-        calculator.result = acc;
-      } else {
-        acc = 0;
-      }
-    return n;
-    }
-    return n;
-  },
-
-  divide: function(a) {
-    var acc;
-    if (a === undefined) {
-      acc = 0;
-    } else {
-      acc = calculator.result / a;
-      calculator.result = acc;
-    }
-    let n = (b) => {
-      if (b !== undefined) {
-        acc = acc / b;
-        calculator.result = acc;
-      } else {
-        acc = 0;
-      }
-    return n;
-    }
-    return n;
-  },
-
-  multiply: function(a) {
-    var acc;
-    if (a === undefined) {
-      acc = 0;
-    } else {
-      acc = calculator.result * a;
-      calculator.result = acc;
-    }
-    let n = (b) => {
-      if (b !== undefined) {
-        acc *= b;
-        calculator.result = acc;
-      } else {
-        acc = 0;
-      }
-    return n;
-    }
-    return n;
-  },
-} 
+const Calculator = () => {
+  let result = 0;
   
-  module.exports = calculator;
+  const getResult = () => {        
+    return result;
+  };
+
+  const reset = () => {
+    result = 0;
+    return result;
+  };
+    
+  const add = (value) => {        
+    result += value;
+    return add;
+  };
+
+  const subtract = (value) => {        
+    result -= value;
+    return subtract;
+  };
+
+  const divide = (value) => {        
+    result /= value;
+    return divide;
+  };
+
+  const multiply = (value) => {        
+    result *= value;
+    return multiply;
+  };
+  
+  return { add, getResult, reset };
+ }
+
+module.exports = calculator;
