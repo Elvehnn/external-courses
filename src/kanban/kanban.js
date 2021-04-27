7fbd9dc9d015b0d8daf09706445fdd7987cd6e04
Elena Shashina <elvehnn@yandex.ru>
2021-04-27 11:16:22 +0300

    fixed classnames and formatting

--- src/kanban/kanban.js
+++ src/kanban/kanban.js
@@ -1,14 +1,15 @@
 const userMenu = document.querySelector('.user__menu');
 const menuArrow = document.querySelector('.menu__arrow');
 
-let dropDownTemplate = `<li class="drop-down__item">Profile</li>
-<li class="drop-down__item">My tasks</li>
-<li class="drop-down__item">Log out</li>`;
+let dropDownTemplate = `
+    <li class="drop-down__item">Profile</li>
+    <li class="drop-down__item">My tasks</li>
+    <li class="drop-down__item">Log out</li>
+`;
 
 let dropDownMenu = document.createElement('ul');
 dropDownMenu.className = "drop-down";
 dropDownMenu.innerHTML = dropDownTemplate;
-
 
 const userMenuClickHandler = () => {
     userMenu.addEventListener("click", () => {
