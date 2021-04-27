1d0859e759df767a54e4fb852212ca249f28f747
Elena Shashina <elvehnn@yandex.ru>
2021-04-27 12:29:14 +0300

    fixed styles and js

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -26,8 +26,7 @@ const petsList = [
     },
 ];
 
-const prevButtonHandler = () => {
-    document.querySelector(".arrow__left").addEventListener("click", () => {
+document.querySelector(".arrow__left").addEventListener("click", () => {
       if (slideIndex > 1) {
         slideIndex -= 1;
         showSlides(petsList[slideIndex - 1]);
@@ -36,10 +35,8 @@ const prevButtonHandler = () => {
         showSlides(petsList[slideIndex - 1]);
       }
     });
-}
 
-const nextButtonHandler = () => {
-    document.querySelector(".arrow__right").addEventListener("click", () => {
+document.querySelector(".arrow__right").addEventListener("click", () => {
       if (slideIndex < petsList.length) {
         slideIndex += 1;
         showSlides(petsList[slideIndex - 1]);
@@ -48,21 +45,9 @@ const nextButtonHandler = () => {
         showSlides(petsList[slideIndex - 1]);
       }
     });
+
+showSlides = (pet) => {
+    let div = document.querySelector(".pet__card");
+    div.innerHTML = '';
+    div.innerHTML = `<img class="pet__picture" src="${pet.img}" alt="${pet.name}"/>`;  
 }
-
-const createElement = (index) => {
-    let str = `<div class="pet__card" id="pet__card">
-    <img class="pet__picture" src="${petsList[index].img}" alt="${petsList[index].name}"/>
-</div>`;
-    return str;
-};
-
-const showSlides = () => {
-    const elem = document.querySelector("#pet__card");
-    elem.innerHTML = '';
-    elem.innerHTML = createElement(slideIndex - 1);
-};
-
-showSlides();
-nextButtonHandler();
-prevButtonHandler();
