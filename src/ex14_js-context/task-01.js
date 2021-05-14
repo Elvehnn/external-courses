cd1807a3878f8d187d968498d114b7739ab691ec
Сидоров Артем Михайлович <Artem_Sidorov@epam.com>
2021-05-14 19:06:45 +0300

    ex-14 is done

---
+++ src/ex14_js-context/task-01.js
@@ -0,0 +1,45 @@
+const Calculator = {
+  result: 0,
+
+  getResult() {
+    return this.result;
+  },
+
+  reset() {
+    this.result = 0;
+    return this;
+  },
+
+  add(value) {        
+    this.result += value;
+    return this;
+  },
+
+  multiply(value) {        
+    this.result *= value;
+    return this;
+  },
+
+  subtract(value) {        
+    this.result -= value;
+    return this;
+  },
+
+  divide(value) {        
+    this.result /= value;
+    return this;
+  },
+
+  setState(value) {
+    this.result = value;
+    return this;
+  },
+
+  fetchData(callback) {
+      this.result = callback(500);
+      return this;
+  },
+};
+
+const calculator = new Calculator;
+module.exports = calculator;
