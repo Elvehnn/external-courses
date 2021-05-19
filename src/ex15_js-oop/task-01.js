033d4ab08bdbeb7650d5ce93485f2c6617338036
Elena Shashina <elvehnn@yandex.ru>
2021-05-19 17:56:14 +0300

    start implement task-01: add constructors, prototipes, main object and 2 methods

--- src/ex15_js-oop/task-01.js
+++ src/ex15_js-oop/task-01.js
@@ -1,1 +1,51 @@
+function Candy(name, weight) {
+    this.name = name;
+    this.weight = weight;
+}
+  
+Candy.prototype.getWeight = function() {
+    console.log(`Вес: ${this.weight}`);
+};
+Candy.prototype.getName = function() {
+    console.log(`Название: ${this.name}`);
+};
 
+function Chocolate(name, weight, topping) {
+    this.type = 'chocolate';
+    this.topping = topping;
+    Candy.call(this, name, weight);
+}
+
+Chocolate.prototype = Candy.prototype;
+
+function Lollipop(name, weight, topping) {
+    this.type = 'lollipop';
+    this.topping = topping;
+    Candy.call(this, name, weight);
+}
+
+Lollipop.prototype = Candy.prototype;
+
+const gift = [];
+gift.push(new Chocolate('Belochka', 30, 'nuts'));
+gift.push(new Chocolate('Red Riding Hood', 20, 'waffle'));
+gift.push(new Chocolate('Bear in the north', 20, 'waffle'));
+gift.push(new Lollipop('Golden key', 5, 'melted sugar'));
+
+
+let totalWeight = gift.reduce((sum, item) => sum + item.weight, 0);
+// console.log(`Общий вес подарка: ${totalWeight} г`);
+
+let sortedGift = (param) => {
+  if (typeof(param) === 'string') {
+    return gift.sort(function (a, b) {
+      return a[param] < b[param];
+    });
+  }
+};
+console.log(sortedGift(name));
+
+let findCandy = (string) => {
+  return gift.find(item => item.name === string);
+};
+// console.log(findCandy('Golden key'));
